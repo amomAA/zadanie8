92f8a3ed16291ea3adcd2ce52021146fcadde74e
Vladimir Chernov <113360964+amomAA@users.noreply.github.com>
2022-11-20 21:50:01 +0300

    Update js.js

--- js.js
+++ js.js
@@ -10,18 +10,20 @@ let exit = document.getElementById("exit");
 
 window.addEventListener("DOMContentLoaded",  
     function (event) {
-       button.addEventListener("click", function (event) { 
+       button.addEventListener("click", function (event) {
+           history.pushState(null, "", "form.html");
+
+           window.addEventListener("popstate", (event) => {
+            popup.style.display = "none";
+            popupwrapper.style.display = "none";
+            button.style.display = "block";  });
+
            popup.style.display = "block"; 
            popupwrapper.style.display = "block";
            button.style.display = "none";
     });
-    exit.addEventListener("click", function (event) { 
-        popup.style.display = "none";
-        popupwrapper.style.display = "none";
-        button.style.display = "block";
- });
-    
 });
+
 function clear(){ 
     let namefield = document.getElementsByName("name");
     let email = document.getElementsByName("email");
